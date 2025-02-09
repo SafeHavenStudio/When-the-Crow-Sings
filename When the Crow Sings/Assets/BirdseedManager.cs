264a2bae3188de2248e01cd408fc622561916c4b
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-08 22:03:18 -0500

    Update BirdseedManager.cs

--- When the Crow Sings/Assets/BirdseedManager.cs
+++ When the Crow Sings/Assets/BirdseedManager.cs
@@ -15,13 +15,18 @@ public class BirdseedManager : MonoBehaviour
 
         if (targetObject == null)
         {
-            targetObject = Instantiate(targetPrefab,transform);
-            targetObject.transform.position = ((BirdseedController)args.objectArgs[0]).transform.position;
-            Destroy(targetObject, 2f);
+            spawnTarget((BirdseedController)args.objectArgs[0]);
         }
         else
         {
             Debug.Log("Already a target in the scene!");
         }
+    }
+
+    void spawnTarget(BirdseedController birdseedController)
+    {
+        targetObject = Instantiate(targetPrefab, transform);
+        targetObject.transform.position = birdseedController.transform.position;
+        Destroy(targetObject, 2f); // Temporary Destroy(). To be removed.
     }
 }
