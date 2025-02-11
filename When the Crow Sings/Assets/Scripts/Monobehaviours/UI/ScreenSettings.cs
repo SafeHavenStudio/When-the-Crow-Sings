64a162cd87e24be4f445d10e1776a97fdd5c3124
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2025-02-11 13:07:54 -0500

    Gamma almost works

--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/ScreenSettings.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/ScreenSettings.cs
@@ -1,29 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class ScreenSettings : MonoBehaviour
 {
     Resolution[] resolutions;
     public TMP_Dropdown resolutionDropdown;
+    public Slider brightnessSlider;
+    public Volume volume;
+    public LiftGammaGain liftGammaGain;
 
     private void Start()
     {
+        volume = FindObjectOfType<Volume>();
+        if (volume != null) liftGammaGain = FindObjectOfType<LiftGammaGain>();
+
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && 
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void setBrightness(float brightnessIndex)
+    {
+        liftGammaGain.gamma.value = new Vector4(brightnessIndex, brightnessIndex, brightnessIndex, 1f);
+        brightnessSlider.value = brightnessIndex;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void setQuality(int qualityIndex)
