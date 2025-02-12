3d70f923f2cba27dfc6dd7993e45a669df6f168d
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-12 01:24:28 -0500

    Singletonification

--- When the Crow Sings/Assets/GlobalVolumeManager.cs
+++ When the Crow Sings/Assets/GlobalVolumeManager.cs
@@ -5,19 +5,10 @@ using UnityEngine.Rendering;
 
 public class GlobalVolumeManager : MonoBehaviour
 {
-    private static GlobalVolumeManager instance;
     public static Volume volume;
 
     private void Awake()
     {
-        if (instance != null)
-        {
-            Destroy(gameObject); // Prevent duplicate volumes
-            return;
-        }
-
-        instance = this;
         volume = GetComponent<Volume>();
-        DontDestroyOnLoad(gameObject); // Keep across scenes
     }
 }
--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/MainMenu/MainMenuDebugLoadHolderProtector.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/MainMenu/MainMenuDebugLoadHolderProtector.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class MainMenuDebugLoadHolderProtector : MonoBehaviour
 {
     public MainMenuDebugLoadHolder mainMenuDebugLoadHolder;
-    private static MainMenuDebugLoadHolderProtector instance;
-
-    void Awake()
-    {
-        if (instance != null) Destroy(gameObject);
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
 }
