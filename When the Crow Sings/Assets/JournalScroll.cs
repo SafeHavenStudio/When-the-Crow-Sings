bf38e8b0fd7830f413a6dfa26b65b30c1bf6e6bf
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-12 18:11:11 -0500

    Reset scrollbar

--- When the Crow Sings/Assets/JournalScroll.cs
+++ When the Crow Sings/Assets/JournalScroll.cs
@@ -12,17 +12,24 @@ public class JournalScroll : MonoBehaviour
     private void OnEnable()
     {
         InputManager.playerInputActions.UI.Journal_Scroll.performed += OnJournalScrollPerformed;
-        scrollbar.value = 0;
+        StartCoroutine(ResetScrollbarAfterOneFrameBecauseUnityIsDumb());
     }
     private void OnDisable()
     {
         InputManager.playerInputActions.UI.Journal_Scroll.performed -= OnJournalScrollPerformed;
     }
 
+    IEnumerator ResetScrollbarAfterOneFrameBecauseUnityIsDumb()
+    {
+        yield return null;
+        scrollbar.value = 1.0f;
+    }
+
     void OnJournalScrollPerformed(InputAction.CallbackContext context)
     {
-        Debug.Log("Scroll scroll scroll your boat..." + context.ReadValue<float>().ToString());
+        
         if (context.ReadValue<float>() != 0f)
             scrollbar.value = Mathf.Clamp01(scrollbar.value + Mathf.Sign(context.ReadValue<float>())*speedMult*scrollbar.size);
+        Debug.Log("Scroll scroll scroll your boat..." + scrollbar.value.ToString());
     }
 }
