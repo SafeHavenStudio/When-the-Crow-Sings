8579b63f8d72ddf3119670d629850293b9867808
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-17 01:35:31 -0500

    Game over seems to work, fixed issue with using Awake() on load >:(

--- When the Crow Sings/Assets/Cinematic_SCN_Manager.cs
+++ When the Crow Sings/Assets/Cinematic_SCN_Manager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class Cinematic_SCN_Manager : MonoBehaviour
 {
     public List<GameObject> cutscenes = new List<GameObject>();
+    public GameObject gameOverScreen;
 
     public enum DesiredBehavior
     {
@@ -21,12 +22,13 @@ public class Cinematic_SCN_Manager : MonoBehaviour
     public static void LoadCinematicScene(DesiredBehavior _desiredBehavior)
     {
         desiredBehavior = _desiredBehavior;
+        Debug.Log("Load() Desired behavior is " + desiredBehavior.ToString());
         SceneManager.LoadScene("Cinematic_SCN"); // Not using a scenereference because it'll only ever be this one spot.
-        Debug.Log("Desired behavior is " +  desiredBehavior.ToString());
     }
 
-    private void Awake()
+    private void Start()
     {
+        Debug.Log("Awake() Desired behavior is " + desiredBehavior.ToString());
         switch (desiredBehavior)
         {
             case DesiredBehavior.CUTSCENE_0:
@@ -34,23 +36,29 @@ public class Cinematic_SCN_Manager : MonoBehaviour
                 cutscenes[0].GetComponent<CutsceneManager>().cutsceneFinished.AddListener(LoadMain_SCN);
                 break;
             case DesiredBehavior.GAME_OVER:
-                Debug.Log("YOU DIED");
+                gameOverScreen.SetActive(true);
                 break;
             default: // This should include DesiredBehavior.NONE
                 throw new System.Exception("Cinematic_SCN WAS NOT CORRECTLY LOADED, PLEASE USE LoadCinematicScene() AND PASS A VALID DesiredBehavior.");
                 break;
         }
+        desiredBehavior = DesiredBehavior.NONE;
     }
 
     private void OnDestroy()
     {
-        desiredBehavior = DesiredBehavior.NONE;
+        //desiredBehavior = DesiredBehavior.NONE;
+    }
+
+
+    public void OnGameOverButtonPressed()
+    {
+        //SaveDataAccess.ReadDataFromDisk();
+        LoadMain_SCN();
     }
 
     public void LoadMain_SCN()
     {
-        // TODO: End it.
-        Debug.Log("End of cutscene.");
         SceneManager.LoadScene(mainScene.Name);
     }
 }
