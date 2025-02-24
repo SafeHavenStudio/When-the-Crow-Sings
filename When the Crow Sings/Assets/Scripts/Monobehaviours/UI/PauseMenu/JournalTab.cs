a534456ef0ef37354c787660b6c62a6c2eba7aa0
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-23 22:14:08 -0500

    Moved tab stuff to the abstract class

--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/JournalTab.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/JournalTab.cs
@@ -1,10 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public abstract class JournalTab : MonoBehaviour
+public abstract class JournalTab : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public abstract void SetActivateTab(bool activated);
-    public abstract void ActivateTab();
-    public abstract void DeactivateTab();
+    public Animator animator;
+    public Sprite activatedSprite;
+    public Sprite deactivatedSprite;
+    public Image image;
+
+    public List<JournalTabSide> journalTabSides = new List<JournalTabSide>();
+
+    public bool isActivated = false;
+
+    public virtual void SetActivateTab(bool activated)
+    {
+        animator.SetBool("isActivated", activated);
+
+        isActivated = activated;
+    }
+    public virtual void ActivateTab()
+    {
+        Debug.Log("Ping!");
+        image.sprite = activatedSprite;
+        SetActivateTab(true);
+    }
+
+    public virtual void DeactivateTab()
+    {
+        Debug.Log("!gniP");
+        image.sprite = deactivatedSprite;
+        SetActivateTab(false);
+    }
+
+    private void OnEnable()
+    {
+        image.sprite = deactivatedSprite;
+        //StartCoroutine(wait());
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1f);
+        SetActivateTab(!isActivated);
+        StartCoroutine(wait());
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        ActivateTab();
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        DeactivateTab();
+    }
+    public void OnButtonPressed()
+    {
+        foreach (JournalTabSide i in journalTabSides)
+        {
+
+        }
+    }
 }
--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/JournalTabSide.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/JournalTabSide.cs
@@ -4,67 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class JournalTabSide : JournalTab, IPointerEnterHandler, IPointerExitHandler
+public class JournalTabSide : JournalTab//, IPointerEnterHandler, IPointerExitHandler
 {
-    public Sprite activatedSprite;
-    public Sprite deactivatedSprite;
 
-    public Animator animator;
-
-    public Image image;
-
-
-    public bool isActivated = false;
-    public override void SetActivateTab(bool activated)
-    {
-        animator.SetBool("isActivated", activated);
-        
-        isActivated = activated;
-    }
-
-    public override void ActivateTab()
-    {
-        Debug.Log("Ping!");
-        image.sprite = activatedSprite;
-        SetActivateTab(true);
-    }
-
-    public override void DeactivateTab()
-    {
-        Debug.Log("!gniP");
-        image.sprite = deactivatedSprite;
-        SetActivateTab(false);
-    }
-
-    private void OnEnable()
-    {
-        image.sprite = deactivatedSprite;
-        //StartCoroutine(wait());
-    }
-
-    IEnumerator wait()
-    {
-        yield return new WaitForSeconds(1f);
-        SetActivateTab(!isActivated);
-        StartCoroutine(wait());
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        ActivateTab();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        DeactivateTab();
-    }
-
-    public List<JournalTabSide> journalTabSides = new List<JournalTabSide>();
-    public void OnButtonPressed()
-    {
-        foreach (JournalTabSide i in journalTabSides)
-        {
-
-        }
-    }
 }
