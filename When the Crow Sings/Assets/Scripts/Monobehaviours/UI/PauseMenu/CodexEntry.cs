701ea97f1a468219f8d9ef76660e9ee17d5d4527
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-02-26 20:19:51 -0500

    Codex works.

--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/CodexEntry.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/CodexEntry.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodexEntry : MonoBehaviour
 {
@@ -10,17 +11,12 @@ public class CodexEntry : MonoBehaviour
     public Sprite discoveredImage;
 
     public string undiscoveredName = "Unknown";
-
-
-
     public string characterName = "missingno.";
 
-    [TextArea]
-    public string undiscoveredDescription = "NoMessageWritten.";
-    [TextArea]
-    public string characterDescription = "NoMessageWritten";
-    [TextArea]
-    public string additionalDescription = "NoMessageWritten";
+    [TextArea] public string undiscoveredDescription = "NoMessageWritten.";
+    [TextArea] public string undiscoveredAdditionalDescription = "Nothing more is known at this time.";
+    [TextArea] public string characterDescription = "NoMessageWritten";
+    [TextArea] public string additionalDescription = "NoMessageWritten";
 
     public string hasBeenHeardOf = "TestingFlag1";
     public string hasBeenMetFlag = "TestingFlag1";
@@ -28,18 +24,26 @@ public class CodexEntry : MonoBehaviour
 
     public TextMeshProUGUI characterNameLabel;
     public TextMeshProUGUI characterDescriptionLabel;
+    public TextMeshProUGUI characterDescriptionLabel2;
+    public Image imageComponent;
 
     private void OnEnable()
     {
         if (SaveDataAccess.saveData.boolFlags[hasBeenFinishedFlag])
         {
+            imageComponent.sprite = discoveredImage;
+
             characterNameLabel.text = characterName;
             characterDescriptionLabel.text = characterDescription;
+            characterDescriptionLabel2.text = additionalDescription;
         }
         else
         {
+            imageComponent.sprite = undiscoveredImage;
+
             characterNameLabel.text = undiscoveredName;
             characterDescriptionLabel.text = undiscoveredDescription;
+            characterDescriptionLabel2.text = undiscoveredAdditionalDescription;
         }
     }
 
