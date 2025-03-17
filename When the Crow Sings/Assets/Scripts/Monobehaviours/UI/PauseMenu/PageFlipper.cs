5fd2f6dc5e8165c631a1053040eb140619ee8beb
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-03-17 00:20:00 -0400

    Start flipping visuals architecture

---
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/PageFlipper.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageFlipper : MonoBehaviour
+{
+    public void FlipRight()
+    {
+
+    }
+    public void FlipLeft()
+    {
+
+    }
+}
