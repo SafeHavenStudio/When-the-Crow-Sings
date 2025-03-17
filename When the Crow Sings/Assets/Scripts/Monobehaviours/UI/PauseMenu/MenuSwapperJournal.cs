8756d9e9421488736c931ff5814dadb359277761
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-03-17 00:23:23 -0400

    PageFlipper has some temp stuff

--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/MenuSwapperJournal.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/MenuSwapperJournal.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MenuSwapperJournal : MenuSwapper
 {
+    public PageFlipper pageFlipper;
     protected override void AdditionalMenuLogic(int whichMenu)
     {
         // TODO: try to figure out the easiest way to put this in a child class maybe so it's not clogging up everything else...
