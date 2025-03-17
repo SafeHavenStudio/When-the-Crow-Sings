7bd2a66837033fb4c46c9907cbe6377f2b0d08d0
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-03-17 01:59:04 -0400

    Take screenshot of isolated area

--- When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/PageFlipper.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/UI/PauseMenu/PageFlipper.cs
@@ -36,14 +36,12 @@ public class PageFlipper : MonoBehaviour
     {
         Texture2D screenshotRaw = ScreenCapture.CaptureScreenshotAsTexture();
         Texture2D screenshotProcessed = new Texture2D(screenshotRaw.width, screenshotRaw.height, TextureFormat.RGB24, false);
-        //Texture2D screenshotProcessed = new Texture2D((int)image1.rectTransform.sizeDelta.x, (int)image1.rectTransform.sizeDelta.y, TextureFormat.RGB24, false);
         screenshotProcessed.SetPixels(screenshotRaw.GetPixels());
         screenshotProcessed.Apply();
         Destroy(screenshotRaw);
 
-        Sprite screenshotSprite = Sprite.Create(screenshotProcessed, new Rect(0, 0, screenshotProcessed.width, screenshotProcessed.height), new Vector2(0.5f, 0.5f));
-        //Sprite screenshotSprite = Sprite.Create(screenshotProcessed, new Rect(image1.rectTransform.position.x, image1.rectTransform.position.y, (int)image1.rectTransform.sizeDelta.x, (int)image1.rectTransform.sizeDelta.y), new Vector2(0.5f, 0.5f));
-        //Sprite screenshotSprite = Sprite.Create(screenshotProcessed, new Rect(0, 0, (int)image1.rectTransform.sizeDelta.x, (int)image1.rectTransform.sizeDelta.y), new Vector2(0.5f, 0.5f));
+        Sprite screenshotSprite = Sprite.Create(screenshotProcessed, new Rect(-image1.rectTransform.anchoredPosition.x, -image1.rectTransform.anchoredPosition.y,
+            (int)image1.rectTransform.sizeDelta.x, (int)image1.rectTransform.sizeDelta.y), new Vector2(0.5f, 0.5f));
 
         return screenshotSprite;
     }
