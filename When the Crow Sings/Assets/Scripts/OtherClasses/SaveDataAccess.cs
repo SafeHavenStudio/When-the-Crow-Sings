68d1e84e8cdc7e0b1a48676fff749c310c507646
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-03-24 22:37:27 -0400

    Additional logic in softlock check

--- When the Crow Sings/Assets/Scripts/OtherClasses/SaveDataAccess.cs
+++ When the Crow Sings/Assets/Scripts/OtherClasses/SaveDataAccess.cs
@@ -66,9 +66,10 @@ public class SaveDataAccess
                     (saveData.intFlags["day"] == 2)
                     &&
                     ((saveData.boolFlags["AngelBaseCompleted"] ? 1 : 0) +
-                    (saveData.boolFlags["BeauBaseCompleted"] ? 1 : 0) +
-                    (saveData.boolFlags["CalebBaseCompleted"] ? 1 : 0)
-                    == 1)
+                        (saveData.boolFlags["BeauBaseCompleted"] ? 1 : 0) +
+                        (saveData.boolFlags["CalebBaseCompleted"] ? 1 : 0)
+                        == 1)
+                    && (saveData.boolFlags["FaridaBaseCompleted"] == false)
                     )
         { saveData.boolFlags["SoftlockCheckFlag1"] = true; }
         else
