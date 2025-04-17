13788347c4ac751f0f16a4c222564cb5b25d0018
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-04-17 17:31:52 -0400

    Improvement to journal tab animations

--- When the Crow Sings/Assets/Scripts/JournalTabTopsHolder.cs
+++ When the Crow Sings/Assets/Scripts/JournalTabTopsHolder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class JournalTabTopsHolder : MonoBehaviour
 {
     public List<GameObject> objectsToCheckIfEnabled;
+
+    public List<GameObject> tabs;
 
     void Update()
     {
@@ -19,6 +22,35 @@ public class JournalTabTopsHolder : MonoBehaviour
         }
 
         GetComponent<Animator>().SetBool("shouldBeVisible", objectIsEnabled);
+
+        SetTabs(objectIsEnabled);
+    }
+
+    void SetTabs(bool objectIsEnabled)
+    {
+        if (objectIsEnabled)
+        {
+            foreach (GameObject i in tabs)
+            {
+                i.SetActive(objectIsEnabled);
+            }
+        }
+        else
+        {
+            if (!disableTabsIsRunning)
+                StartCoroutine(DisableTabs());
+        }
         
+    }
+    bool disableTabsIsRunning = false;
+    IEnumerator DisableTabs()
+    {
+        disableTabsIsRunning = true;
+        yield return new WaitForSecondsRealtime(.1f);
+        foreach (GameObject i in tabs)
+        {
+            i.SetActive(false);
+        }
+        disableTabsIsRunning = false;
     }
 }
