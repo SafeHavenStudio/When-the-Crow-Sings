d8e7d6a11c1168d8627320307a6f057127edcd60
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-04-21 03:12:17 -0400

    Smooth scroll for better or worse

--- When the Crow Sings/Assets/Scripts/JournalScroll.cs
+++ When the Crow Sings/Assets/Scripts/JournalScroll.cs
@@ -9,6 +9,8 @@ public class JournalScroll : MonoBehaviour
     public Scrollbar scrollbar;
     public float speedMult = .1f;
 
+    float currentDirection = 0f;
+
     private void OnEnable()
     {
         InputManager.playerInputActions.UI.Journal_Scroll.performed += OnJournalScrollPerformed;
@@ -17,6 +19,15 @@ public class JournalScroll : MonoBehaviour
     private void OnDisable()
     {
         InputManager.playerInputActions.UI.Journal_Scroll.performed -= OnJournalScrollPerformed;
+        //InputManager.playerInputActions.UI.Journal_Scroll.IsPressed();
+    }
+
+    private void Update()
+    {
+        if (!InputManager.playerInputActions.UI.Journal_Scroll.IsPressed()) currentDirection = 0f;
+
+        scrollbar.value = Mathf.Clamp01(scrollbar.value + (currentDirection * speedMult * scrollbar.size * Time.unscaledDeltaTime));
+        Debug.Log("Current scroll direction is " + currentDirection.ToString() + " Scrolling to " + scrollbar.value.ToString());
     }
 
     IEnumerator ResetScrollbarAfterOneFrameBecauseUnityIsDumb()
@@ -27,9 +38,11 @@ public class JournalScroll : MonoBehaviour
 
     void OnJournalScrollPerformed(InputAction.CallbackContext context)
     {
-        
-        if (context.ReadValue<float>() != 0f)
-            scrollbar.value = Mathf.Clamp01(scrollbar.value + Mathf.Sign(context.ReadValue<float>())*speedMult*scrollbar.size);
-        Debug.Log("Scroll scroll scroll your boat..." + scrollbar.value.ToString());
+
+        //if (context.ReadValue<float>() != 0f)
+        //    scrollbar.value = Mathf.Clamp01(scrollbar.value + Mathf.Sign(context.ReadValue<float>())*speedMult*scrollbar.size);
+        //Debug.Log("Scroll scroll scroll your boat..." + scrollbar.value.ToString());
+        if (context.ReadValue<float>() != 0f) currentDirection = Mathf.Sign(context.ReadValue<float>());
+        else currentDirection = 0f;
     }
 }
