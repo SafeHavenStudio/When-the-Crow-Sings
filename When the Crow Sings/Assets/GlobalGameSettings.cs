cdc3207018e7647c245bb6413cbffe465a6197fc
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-04-22 04:09:51 -0400

    Add settings to singleton

--- When the Crow Sings/Assets/GlobalGameSettings.cs
+++ When the Crow Sings/Assets/GlobalGameSettings.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class GlobalGameSettings : MonoBehaviour
 {
-    static string exampleSetting = "Hey, Ricky! This is an example setting.";
+    public static float masterVolume = 1.0f;
+    public static float musicVolume = 1.0f;
+    public static float ambienceVolume = 1.0f;
+    public static float soundFxVolume = 1.0f;
+    public static float voicesVolume = 1.0f;
+
+    public static int autoRun = 0;
+    public static int qteSpeed = 4;
+    public static int qteDecay = 1;
+    public static float textSpeed = 1.0f;
+
+    public static int graphicsQualityIndex = 1;
+    public static int screenResolutionIndex = 0;
+    public static float screenBrightness = 0.4f;
+
+    private void Start()
+    {
+        
+    }
+
+    //public static void ResetPlayerPrefs()
+    //{
+
+    //}
+
+    public static void ErasePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    public static void SavePlayerPrefs()
+    {
+        PlayerPrefs.Save();
+    }
 }
